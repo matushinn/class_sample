3a66bfffe0a5de033da1a729b4b3c5d6a3ad5797
shotaro-ooe <shoooe97@gmail.com>
2018-11-18 14:19:35 +0900

    インスタンス化

--- user.py
+++ user.py
@@ -1,13 +1,29 @@
 class User:
-    def __init__(self):
-        self.name = "Bob"
+    def __init__(self,name,age,country):
+        self.name = name
+        self.age = age
+        self.country = country
 
 
 if __name__ == '__main__':
-    user1 = User() #Userクラスをインスタンス化
+    Bob = User("Bob", 15, "America")
+    #Userクラスをインスタンス化
 
-    print(user1)
-    print(user1.name)
+    print(Bob)
+    print(Bob.name)
+    print(Bob.age)
+    print(Bob.country)
 
-if __name__ == "__main__":
+
+    Tom = User("Tom", 57, "America")
+    print(Tom)
+    print(Tom.name)
+    print(Tom.country)
+
+    Ken = User("Ken", 49, "Japan")
+    print(Ken)
+    print(Ken.name)
+    print(Ken.country)
+
+
 
