404951c5806954be72d3b3715c1465eab187fe8f
shotaro-ooe <shoooe97@gmail.com>
2018-11-18 13:57:43 +0900

    main

---
+++ user.py
@@ -0,0 +1,13 @@
+class User:
+    def __init__(self):
+        self.name = "Bob"
+
+
+if __name__ == '__main__':
+    user1 = User() #Userクラスをインスタンス化
+
+    print(user1)
+    print(user1.name)
+
+if __name__ == "__main__":
+
