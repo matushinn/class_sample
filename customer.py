704da32d122266942d61ea97153414a1f6b7cc8a
shotaro-ooe <shoooe97@gmail.com>
2018-11-18 16:46:26 +0900

    first commit

--- customer.py
+++ customer.py
@@ -1,16 +1,29 @@
 class Customer:
-    def __init__(self,first_name,family_name,age):
+    def __init__(self, first_name, family_name, age):
         self.first_name = first_name
         self.family_name = family_name
         self.age = age
 
     def full_name(self):
+        # return self.first_name + self.family_name
+        return self.family_name + self.first_name
 
-       return self.first_name + self.family_name
+    def display_profile(self):
+        print(f"Name: {self.full_name()}, Age: {self.age}")
 
- if __name__ == "__main__":
-    tom = Customer("Tom","Ford",57)
-    print(tom.full_name())
+    def display_full_name(self):
+        print(self.full_name())
 
+
+if __name__ == "__main__":
+    tom = Customer("Tom", "Ford", 57)
     ken = Customer("Ken", "Yokoyama", 49)
-    print(tom.full_name())
+
+    tom.display_profile()
+    ken.display_profile()
+
+    ken.display_full_name()
+
+
+
+
