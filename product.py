aa567c8cb4c7a34ef74705f5f4a462cf1202dbbb
shotaro-ooe <shoooe97@gmail.com>
2018-11-18 15:23:56 +0900

    インスタンス化

---
+++ product.py
@@ -0,0 +1,13 @@
+class Prooduct:
+    def __init__(self,name,price):
+        self.name = name
+        self.price - price
+
+
+if __name__ == "__main__":
+
+  p1 = Prooduct("iPhone",100000) #プロダクトクラスのインスタンス化
+  print(p1)
+
+  print(p1.name) #iPhone
+  print(p1.price)
