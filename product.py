5160858f83d982011f1e3bb548ebf70c76700aed
shotaro-ooe <shoooe97@gmail.com>
2018-11-18 15:53:48 +0900

    discount 追加

--- product.py
+++ product.py
@@ -1,13 +1,27 @@
-class Prooduct:
-    def __init__(self,name,price):
+class Product:
+    def __init__(self, name, amount):
         self.name = name
-        self.price - price
+        self.amount = amount
+
+    def discount(self,price):
+        self.amount -= price
+
+
 
 
 if __name__ == "__main__":
 
-  p1 = Prooduct("iPhone",100000) #プロダクトクラスのインスタンス化
+  p1 = Product("iPhone",100000)
+  #プロダクトクラスのインスタンス化
   print(p1)
 
   print(p1.name) #iPhone
-  print(p1.price)
+  print(p1.amount)
+  p1.discount(5000)
+  print(p1.amount)
+
+  p2 = Product("MacBookAir",150000)
+  print(p2.name)
+  print(p2.amount)
+  p2.discount(9800)
+  print(p2.amount)
